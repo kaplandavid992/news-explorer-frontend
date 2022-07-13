62b20287459c439b306ac2045bb8da808007bac7
kaplandavid992 <kaplandavid992@gmail.com>
2022-07-14 01:45:43 +0300

    initial search form

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
+import SearchForm from '../SearchForm/SearchForm';
 
 function Main() {
     return (
         <>
         <p>this is main component</p>
         <Link to='saved-news'> saved news page </Link>
+        <SearchForm />
         </>
     )
 }
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,20 @@
+import "./SearchForm.css";
+
+function SearchForm() {
+  return (
+    <div className="searchForm">
+      <h1 className="searchForm__h1">What's going on in the world?</h1>
+      <span className="searchForm__span">
+        Find the latest news on any topic and save them in your personal
+        account.
+      </span>
+
+      <form>
+        <input className='searchForm__input' placeholder="Enter topic"></input>
+        <button className="searchForm__btn"></button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchForm;
