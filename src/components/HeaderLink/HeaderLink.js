c1b4b75df224b2b9e485715021f0664dea3c529a
kaplandavid992 <kaplandavid992@gmail.com>
2022-07-26 03:35:41 +0300

    feat:header margins

--- src/components/HeaderLink/HeaderLink.js
+++ src/components/HeaderLink/HeaderLink.js
@@ -1,16 +1,19 @@
 import HeaderLinkUnderline from '../HeaderLinkUnderline/HeaderLinkUnderline';
 import { Link } from 'react-router-dom';
 import "./HeaderLink.css";
+import homeUnderLine from '../../images/homeUnder.svg';
+import saveUnderLine from '../../images/saveUnderBlack.svg';
 
 function HeaderLink({ to, text, sidePadding, color, theme }) {
   const marginLeft = text === 'Saved articles' ? '16' : '0';
+  const underlineSource = text === 'Saved articles' ? saveUnderLine : homeUnderLine;
   const linkLightThemeColor = theme === 'light' ? 'header__nav-link_light' :'';
   return (
     <div className="header__nav-link-wrapper">
       <Link to={to} className={`header__nav-link ${linkLightThemeColor}`}  style={{ paddingRight:sidePadding , paddingLeft:sidePadding, marginLeft:marginLeft }}>
         {text}
       </Link>
-      <HeaderLinkUnderline to={to} marginLeft={marginLeft} marginRight={marginLeft} theme={theme}/>
+      <img src={underlineSource} className='header__nav-link-underline' alt='underline highlighter line' />
     </div>
   );
 }
